37f602dd6ec0c788b298618a0606908a2d4064ae
Shiva-DS24 <doctshind@gmail.com>
2024-06-17 21:50:18 +0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from scipy.stats import skew, kurtosis
 import re
+import os
 
 
 class AssessmentAnalysis:
@@ -19,12 +20,73 @@ class AssessmentAnalysis:
         self.alpha = 0
         self.sem = 0
         self.item_analysis = None
+        self.n_students =0
+        self.n_items=0
+        self.help_text = """
+                        pychometrics Library Help
+                        ---------------------
+                        Available Commands and Syntax:              
+
+
+                        1. AssessmentAnalysis(csv_file name)
+                           - Description: This function provides input data for item analysis. 
+                                Data must be prepared using the template. Once read successfully it creates an object for further processing.
+                           - Syntax: AssessmentAnalysis('data.csv')
+
+                        2. info()
+                           - Description: To get information about the loaded data at any time. 
+                                It displays identified number of students and items. 
+                           - Syntax: analysis.info()
+
+                        3. calc_desc()
+                           - Description: The descriptive statistics of the entire test can be obtained by running.
+                           - Syntax: analysis.calc_desc()
+
+                        4. calc_skew()
+                           - Description: Calculates the skewness of the dataset.
+                           - Syntax: analysis.skew()
+
+                        5. calc_kurt()
+                           - Description: Calculates the kurtosis of the dataset.
+                           - Syntax: analysis.kurt()
+
+                        6. calc_fi()
+                           - Description: Calculates the facility index for all questions.
+                           - Syntax: analysis.calc_fi()
+
+                        7. calc_di()
+                           - Description: Calculates the discrimination index for all questions.
+                           - Syntax: analysis.calc_di()
+                        
+                        8. export_indices()
+                           - Description: Calculates both DI and FI and export the results as csv file.
+                           - Syntax: analysis.export_indices()
+
+                        9. calc_alpha()
+                           - Description: Calculates Cronbach's Alpha for the dataset.
+                           - Syntax: analysis.calc_alpha()
+                        10. calc_sem()
+                           - Description: Calculates SEM for the dataset.
+                           - Syntax: analysis.calc_sem()                  
+
+                        11. export_report()
+                            - Description: A comprehensive pdf report can be generated .
+                            - Syntax: analysis.export_report()
+
+                        """
 
     def filter_columns(self):
         self.df = self.df[self.df['Surname'] != 'Overall average']
         pattern = r'Q\..*/'
         filtered_columns = self.df.filter(regex=pattern).columns
         self.marks_df = self.df.loc[:, filtered_columns].copy()
+
+    def info(self):
+        self.filter_columns()
+        self.n_students = len(self.marks_df.axes[0])
+        self.n_items = len(self.marks_df.axes[1])
+        print("No of students", self.n_students)
+        print("No of items", self.n_items)
 
     def extract_max_marks(self):
         max_marks = {col: float(re.search(r'/([0-9.]+)', col).group(1)) for col in self.marks_df.columns}
@@ -41,7 +103,36 @@ class AssessmentAnalysis:
         self.skewness = round(skew(self.marks_df['Total_Percentage']), 2)
         self.kurt = round(kurtosis(self.marks_df['Total_Percentage']), 2)
 
-    def calculate_cronbach_alpha(self):
+    def calc_desc(self):
+        self.filter_columns()
+        self.extract_max_marks()
+        self.calculate_totals()
+        self.calculate_statistics()
+        self.mean = round(self.marks_df['Total_Percentage'].mean(), 2)
+        self.median = round(self.marks_df['Total_Percentage'].median(), 2)
+        self.std_dev = round(self.marks_df['Total_Percentage'].std(), 2)
+        print("Mean of total score (%)", self.mean)
+        print("Median of total score (%)", self.median)
+        print("Std dev. of total score (%)", self.std_dev)
+
+    def calc_skew(self):
+        self.filter_columns()
+        self.extract_max_marks()
+        self.calculate_totals()
+        skewness = round(skew(self.marks_df['Total_Percentage']), 2)
+        print("Skewness of total score", skewness)
+
+    def calc_kurt(self):
+        self.filter_columns()
+        self.extract_max_marks()
+        self.calculate_totals()
+        kurt_value = round(kurtosis(self.marks_df['Total_Percentage']), 2)
+        print("Kurtosis of total score", kurt_value)
+
+    def calc_alpha(self):
+        self.filter_columns()
+        self.extract_max_marks()
+        self.calculate_totals()
         marks = self.marks_df.drop(columns=['Total', 'Total_Percentage'])
         k = marks.shape[1]
         item_variances = np.var(marks, axis=0, ddof=1)
@@ -51,6 +142,19 @@ class AssessmentAnalysis:
         total_std_dev = np.sqrt(total_variance)
         self.sem = round(total_std_dev * np.sqrt(1 - self.alpha / 100), 2)
 
+    def calc_sem(self):
+        self.filter_columns()
+        self.extract_max_marks()
+        self.calculate_totals()
+        marks = self.marks_df.drop(columns=['Total', 'Total_Percentage'])
+        k = marks.shape[1]
+        item_variances = np.var(marks, axis=0, ddof=1)
+        total_scores = marks.sum(axis=1)
+        total_variance = np.var(total_scores, ddof=1)
+        self.alpha = round((k / (k - 1)) * (1 - (item_variances.sum() / total_variance)) * 100, 2)
+        total_std_dev = np.sqrt(total_variance)
+        self.sem = round(total_std_dev * np.sqrt(1 - self.alpha / 100), 2)
+        print("Std Error of measurement of the test is", self.sem)
     def combine_stats(self):
         self.stats = {
             'mean': self.mean,
@@ -63,6 +167,9 @@ class AssessmentAnalysis:
         }
 
     def calc_indices(self):
+        self.filter_columns()
+        self.extract_max_marks()
+        self.calculate_totals()
         facility_index = {}
         for column_name in self.marks_df.columns[:-2]:  # Excluding 'Total' and 'Total_Percentage'
             max_marks = float(column_name.split('/')[1].strip())
@@ -77,7 +184,7 @@ class AssessmentAnalysis:
             covariance = np.cov(item_responses, total_score, ddof=1)[0, 1]
             mark_variance = np.var(item_responses, ddof=1)
             total_score_variance = np.var(total_score, ddof=1)
-            print('mark_variance', mark_variance)
+            #print('mark_variance', mark_variance)
             if np.isnan(covariance) or np.isnan(mark_variance) or np.isnan(total_score_variance):
                 discrimination_index = np.nan
             elif mark_variance == 0 or total_score_variance == 0:
@@ -140,6 +247,33 @@ class AssessmentAnalysis:
 
         self.item_analysis = item_analysis
 
+    def calc_fi(self):
+        self.calc_indices()
+        fi = self.item_analysis.iloc[:, 0]
+        print("Facility Indices")
+        print(fi)
+
+    def calc_di(self):
+        self.calc_indices()
+        di = self.item_analysis.iloc[:, 2]
+        print("Discrimination Indices")
+        print(di)
+
+    def export_indices(self):
+        self.calc_indices()
+        base_filename = "indices.csv"
+        counter = 1
+        filename = base_filename
+
+        # Check if the file already exists
+        while os.path.exists(filename):
+            filename = f"indices{counter}.csv"
+            counter += 1
+
+        # Export DataFrame to CSV with the unique filename
+        self.item_analysis.to_csv(filename, index=False)
+        print(f"{filename} is created in the working directory.")
+
     def generate_report(self):
         from .report import generate_pdf_report
         generate_pdf_report(self.marks_df, self.total_max_marks, self.item_analysis, self.stats)
@@ -149,11 +283,22 @@ class AssessmentAnalysis:
         self.extract_max_marks()
         self.calculate_totals()
         self.calculate_statistics()
-        self.calculate_cronbach_alpha()
+        self.calc_alpha()
         self.combine_stats()
         self.calc_indices()
         self.generate_report()
 
+    def help(self):
+        print(self.help_text)
+
+    def __getattr__(self, attr):
+        if attr == 'help':
+            return self.help
+        else:
+            raise AttributeError(f"'{self.__class__.__name__}' there is no such function currently available: '{attr}'. "
+                                 f"Use analysis.help() for available functions.")
+
 # Example usage:
 # analysis = AssessmentAnalysis('data.csv')
-# analysis.run_analysis()
+# analysis.info()
+
